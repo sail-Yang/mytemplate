3ea12f46ce5fbf0fc2fe333243b7615afcbc9324
sai-Yang <1186599081@qq.com>
2024-06-23 10:57:27 +0800

    feature：admin init

---
+++ admin/src/main/java/org/example/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package org.example.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangfan
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/6/23 10:53
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
